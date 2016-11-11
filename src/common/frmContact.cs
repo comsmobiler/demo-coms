7671d7de290f68b0dc600ac702f3fbebeb397b6e
lincylin <659386849@qq.com>
2016-11-11 15:31:56 +0800

    dmeocoms新增IM功能

---
+++ src/common/frmContact.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMSSmobilerDemo.common
+{
+    class frmContact
+    {
+        /// <summary>
+        /// 获取联系人数据
+        /// </summary>
+        /// <returns></returns>
+        public DataTable GetContactData()
+        {
+
+            DataTable table = new DataTable();
+            table.Columns.Add("UserID", typeof(System.String));
+            table.Columns.Add("PortraitUri", typeof(System.String));
+            table.Rows.Add("A", "A.PNG");
+            table.Rows.Add("B", "B.PNG");
+            return table;
+        }
+
+        /// <summary>
+        /// 获取联系人数据
+        /// </summary>
+        /// <returns></returns>
+        public DataTable GetContactData1()
+        {
+
+            DataTable table = new DataTable();
+            table.Columns.Add("UserID", typeof(System.String));
+            table.Columns.Add("PortraitUri", typeof(System.String));
+            table.Columns.Add("isAddC", typeof(System.Boolean));
+            table.Rows.Add("A", "A.PNG",true );
+            table.Rows.Add("B", "B.PNG", true);
+            table.Rows.Add("C", "C.PNG", false );
+            table.Rows.Add("D", "D.PNG", false);
+            table.Rows.Add("E", "E.PNG", false);
+            return table;
+        }
+    }
+}
---
+++ src/common/frmGroup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMSSmobilerDemo.common
+{
+    class frmGroup
+    {
+        /// <summary>
+        /// 获取群组数据
+        /// </summary>
+        /// <returns></returns>
+        public DataTable GetGroupData()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("G_ID", typeof(System.String));
+            table.Columns.Add("G_NAME", typeof(System.String));
+            table.Rows.Add("G0001", "smobiler交流群");
+            table.Rows.Add("G0002", "smobiler横屏交流群");            
+            return table;
+        }
+
+        /// <summary>
+        /// 获取笔记本数据
+        /// </summary>
+        /// <returns></returns>
+        public DataTable GetGroupUserByID()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("G_ID", typeof(System.String));
+            table.Columns.Add("G_NAME", typeof(System.String));
+            table.Rows.Add("G0001", "smobiler交流群");
+            table.Rows.Add("G0002", "smobiler横屏交流群");
+            return table;
+        }
+    }
+}
